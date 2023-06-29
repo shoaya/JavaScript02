eb5122a21f7e96d404978dd105e5e1c423de9765
ayasho <a.sho@i-t-plus.co.jp>
2023-06-29 14:02:33 +0900

    main.jsのファイル作成

---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function keyUp(e) {
+    let str =inputText.value;
+
+    let num = 10 - str.length;
+
+    console.log(num);
+
+    const characterCount = document.getElementById('characterCount');
+    characterCount.textContent = num;
+}
+
+const inputText = document.getElementById('inputText');
+inputText.addEventListener('keyup', keyUp, false);
+
+
