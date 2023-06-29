8dd68541e06dc17b4dbc5adbe5ce25992cb09b79
ayasho <a.sho@i-t-plus.co.jp>
2023-06-29 15:08:20 +0900

    文字数の変更

--- main.js
+++ main.js
@@ -3,13 +3,17 @@
 function keyUp(e) {
     let str =inputText.value;
 
+
     let num = 10 - str.length;
 
     console.log(num);
 
     const characterCount = document.getElementById('characterCount');
     characterCount.textContent = num;
+    
 }
+    
+    
 
 const inputText = document.getElementById('inputText');
 inputText.addEventListener('keyup', keyUp, false);
